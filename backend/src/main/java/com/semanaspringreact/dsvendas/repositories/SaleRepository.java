23c45bee24f2b7da8f45703aa74cfc7a863ff641
CaioCesarRocha <caio.cesar209@gmail.com>
2021-05-05 20:23:54 -0300

    Group by search

--- backend/src/main/java/com/semanaspringreact/dsvendas/repositories/SaleRepository.java
+++ backend/src/main/java/com/semanaspringreact/dsvendas/repositories/SaleRepository.java
@@ -1,10 +1,21 @@
 package com.semanaspringreact.dsvendas.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.semanaspringreact.dsvendas.dto.SaleSucessDTO;
+import com.semanaspringreact.dsvendas.dto.SaleSumDTO;
 import com.semanaspringreact.dsvendas.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long>{
-
-
+	
+	@Query("SELECT new com.semanaspringreact.dsvendas.dto.SaleSumDTO(obj.seller, SUM(obj.amount))" 
+	+ "FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSumDTO> amountGroupedBySeller();
+	
+	@Query("SELECT new com.semanaspringreact.dsvendas.dto.SaleSucessDTO(obj.seller, SUM(obj.visited), SUM(obj.deals))" 
+	+ "FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSucessDTO> sucessGroupedBySeller();
 }
